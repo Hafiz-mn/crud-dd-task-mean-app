db08c98f2943c021119a376157f7c49a6b48faf4
Hafiz Mohammed <hafizmohammedmn3@gmail.com>
2026-02-24 15:08:16 +0530

    Update main.tf

--- k8s/main.tf
+++ k8s/main.tf
@@ -36,6 +36,9 @@ module "eks" {
     vpc-cni                = {
       before_compute = true
     }
+aws-ebs-csi-driver = {
+    most_recent = true
+  }
   }
 
   vpc_id     = module.vpc.vpc_id
@@ -67,6 +70,7 @@ terraform {
     encrypt        = true
   }
 }
+
 
 
 
