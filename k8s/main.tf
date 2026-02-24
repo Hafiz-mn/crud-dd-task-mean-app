602976873cb731ca639662a0c90d6f8e64c95863
Hafiz Mohammed <hafizmohammedmn3@gmail.com>
2026-02-24 16:51:13 +0530

    Update main.tf

--- k8s/main.tf
+++ k8s/main.tf
@@ -38,7 +38,6 @@ module "eks" {
     }
 aws-ebs-csi-driver = {
     most_recent = true
-service_account_role_arn = aws_iam_role.ebs_csi.arn
   }
   }
 
@@ -71,6 +70,7 @@ terraform {
     encrypt        = true
   }
 }
+
 
 
 
