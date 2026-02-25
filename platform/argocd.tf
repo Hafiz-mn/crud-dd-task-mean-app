5fe0caa864e25ecd667bad21db191362270f2599
Hafiz Mohammed <hafizmohammedmn3@gmail.com>
2026-02-25 08:47:59 +0530

    Update argocd.tf

--- platform/argocd.tf
+++ platform/argocd.tf
@@ -27,9 +27,7 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "5.51.6"
-
-  depends_on = [module.eks]
-
 }
+
 
 
